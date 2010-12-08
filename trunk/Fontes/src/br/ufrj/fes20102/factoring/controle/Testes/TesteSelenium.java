227ad7704392af769fc7cddba1d6770ce758ae81
leticia.brugger@gmail.com <leticia.brugger@gmail.com@71b8dcdf-cc49-b818-70d4-f12ff6e1cee7>
2010-12-08 14:39:19 +0000

    O Teste no Selenium

---
+++ trunk/Fontes/src/br/ufrj/fes20102/factoring/controle/Testes/TesteSelenium.java
@@ -0,0 +1,32 @@
+package br.ufrj.fes20102.factoring.controle.Testes;
+
+import com.thoughtworks.selenium.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.regex.Pattern;
+
+public class TesteSelenium extends SeleneseTestCase {
+	@Before
+	public void setUp() throws Exception {
+		selenium = new DefaultSelenium("localhost", 4444, "*firefox", "http://localhost:8080/") {
+			public void open(String url) {
+				commandProcessor.doCommand("open", new String[] {url, "true"});
+			}
+		};
+		selenium.start();
+	}
+
+	@Test
+	public void testESelenium() throws Exception {
+		selenium.open("/fonte/visao/gerarRelatorioCliente/geraRelatorioClienteForm.jsp");
+		selenium.type("cnpj", "123");
+		selenium.click("//input[@value='Gerar Relatorio']");
+		selenium.waitForPageToLoad("30000");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		selenium.stop();
+	}
+}
