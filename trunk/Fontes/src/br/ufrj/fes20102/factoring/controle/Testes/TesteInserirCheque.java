795d3e882a1564bf07df1eb4ade4682840907052
igor.bread <igor.bread@71b8dcdf-cc49-b818-70d4-f12ff6e1cee7>
2010-12-09 02:13:32 +0000

    Ajustes nos testes.

--- trunk/Fontes/src/br/ufrj/fes20102/factoring/controle/Testes/TesteInserirCheque.java
+++ trunk/Fontes/src/br/ufrj/fes20102/factoring/controle/Testes/TesteInserirCheque.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Calendar;
 
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,18 +12,17 @@ import br.ufrj.fes20102.factoring.controle.Cheque.InsereChequeServlet;
 import br.ufrj.fes20102.factoring.modelo.Dominio.Cheque;
 import br.ufrj.fes20102.factoring.modelo.Persistencia.ChequeDAO;
 
-
 /**
  * 
  * Classe Teste associada ao Caso de Uso de Inserir Cheque
- *
+ * 
  */
 public class TesteInserirCheque {
 
 	private Cheque cheque;
 	private ChequeDAO dao;
 	private InsereChequeServlet servlet;
-	
+
 	@Before
 	public void prepara() {
 		cheque = new Cheque();
@@ -34,53 +32,55 @@ public class TesteInserirCheque {
 		cheque.setData_vencimento(Calendar.getInstance().getTime());
 		cheque.setValor_bruto(98.74);
 		cheque.setValor_descontado(90.5);
-		
+
 		try {
 			dao = new ChequeDAO();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		servlet = new InsereChequeServlet();
 	}
-	
+
 	@After
 	public void limpa() {
 		dao.encerra();
 	}
-	
+
 	@Test
 	public void testaInsereDAO() {
-		
+
 		try {
 			dao.insere(cheque);
-			
+
 			Cheque cheque2 = dao.filtraNumero(cheque.getNumero());
-			
-			assertTrue("Problema em inserir cheque", (cheque2 != null) );
-			
+
+			assertTrue("Problema em inserir cheque", (cheque2 != null));
+
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Test
 	public void testaInsereControle() {
-		
-		boolean faltandoInfo = servlet.validaFormulario("", "", "123", "70", "65", "06/12/2010");
-		
-		assertTrue("Problema em validar os dados", (faltandoInfo == false) );
-		
-		boolean dadosErrados = servlet.validaFormulario("12345", "123", "456", "10", "20", "06/12/2010");
-		
-		assertTrue("Problema em validar os dados", (dadosErrados == false) );
-		
-		boolean clienteInexistente = servlet.validaFormulario("12345", "9", "cliente que nao existe", "80", "70", "06/12/2010");
-		
-		assertTrue("Problema em validar os dados", (clienteInexistente == false) );
-		
+
+		boolean faltandoInfo = servlet.validaFormulario("", "", "123", "70",
+				"65", "06/12/2010");
+
+		assertTrue("Problema em validar os dados", (faltandoInfo == false));
+
+		boolean dadosErrados = servlet.validaFormulario("12345", "123", "456",
+				"10", "20", "06/12/2010");
+
+		assertTrue("Problema em validar os dados", (dadosErrados == false));
+
+		boolean clienteInexistente = servlet.validaFormulario("12345", "9",
+				"cliente que nao existe", "80", "70", "06/12/2010");
+
+		assertTrue("Problema em validar os dados",
+				(clienteInexistente == false));
+
 	}
-	
+
 }
